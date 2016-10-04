3591e99035a49bb254e3fa3f87cc2bf92ccc9de3
homm <homm86@gmail.com>
2016-10-04 13:05:41 +0300

    SIMD. pypi readme, update setup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def get_version():
     return locals()["__version__"]
 
 
-NAME = "Pillow"
+NAME = "Pillow-SIMD"
 PILLOW_VERSION = get_version()
 FREETYPE_ROOT = None
 IMAGEQUANT_ROOT = None
@@ -247,6 +247,7 @@ def _cmd_exists(cmd):
 def _read(file):
     with open(file, "rb") as fp:
         return fp.read()
+
 
 
 def _pkg_config(name):
@@ -725,7 +726,15 @@ class pil_build_ext(build_ext):
         else:
             defs.append(("PILLOW_VERSION", '"%s"' % PILLOW_VERSION))
 
-        exts = [(Extension("PIL._imaging", files, libraries=libs, define_macros=defs))]
+        exts = [
+            Extension(
+                "PIL._imaging",
+                files,
+                libraries=libs,
+                define_macros=defs,
+                extra_compile_args=["-msse4"],
+            )
+        ]
 
         #
         # additional libraries
@@ -853,14 +862,14 @@ try:
         name=NAME,
         version=PILLOW_VERSION,
         description="Python Imaging Library (Fork)",
-        long_description=_read("README.rst").decode("utf-8"),
+        long_description=_read("PyPI.rst").decode("utf-8"),
         license="HPND",
         author="Alex Clark (PIL Fork Author)",
         author_email="aclark@python-pillow.org",
-        url="https://python-pillow.org",
+        url="https://github.com/uploadcare/pillow-simd",
         project_urls={
             "Documentation": "https://pillow.readthedocs.io",
-            "Source": "https://github.com/python-pillow/Pillow",
+            "Source": "https://github.com/uploadcare/pillow-simd",
             "Funding": "https://tidelift.com/subscription/pkg/pypi-pillow",
         },
         classifiers=[
